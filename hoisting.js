0a3d5924c0ec04f3a303ee02a525a5ca96605b2d
emil.huo <emil.huo@gmail.com>
2020-11-17 21:27:40 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -20,12 +20,11 @@ function crazy() {
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-
-
-
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
-  }
   sayMy();
+  function sayMy() {
+   var name = "Kristin";
+    console.log(name);
+    
+  }
+
 }
